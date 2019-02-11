af0a212a63b53562e6c4804355e940e8870e86fa
Cesa Kwaku Salaam <cesa.salaam@gmail.com>
2019-02-10 21:28:34 -0500

    Initial Commit

---
+++ My Lawyer Now - Ios App/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  My Lawyer Now - Ios App
+//
+//  Created by Cesa Salaam on 2/10/19.
+//  Copyright © 2019 Cesa Salaam. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
